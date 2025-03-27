8d3081c3cf2cbf59d290199ab73afa8e9cb11e33
Matt <3422894+mawise@users.noreply.github.com>
2025-03-27 17:02:58 -0500

    per-bike view

--- scatter.rb
+++ scatter.rb
@@ -5,22 +5,21 @@ def map_to_range(value, in_min, in_max, out_min, out_max)
 end
 
 def draw_chart(data_series)
+
+  ## Find max and min values so we know what area to graph
   x_min = nil
   x_max = nil
   y_min = nil
   y_max = nil
-
-  data_series.each do |series|
-    series.each do |point|
-      x_min = point[0] if x_min.nil?
-      x_max = point[0] if x_max.nil?
-      y_min = point[1] if y_min.nil?
-      y_max = point[1] if y_max.nil?
-      x_min = point[0] if point[0] < x_min
-      x_max = point[0] if point[0] > x_max
-      y_min = point[1] if point[1] < y_min
-      y_max = point[1] if point[1] > y_max
-    end
+  data_series.each do |point|
+    x_min = point[0] if x_min.nil?
+    x_max = point[0] if x_max.nil?
+    y_min = point[1] if y_min.nil?
+    y_max = point[1] if y_max.nil?
+    x_min = point[0] if point[0] < x_min
+    x_max = point[0] if point[0] > x_max
+    y_min = point[1] if point[1] < y_min
+    y_max = point[1] if point[1] > y_max
   end
   x_min -= (x_max - x_min)*0.1
   x_max += (x_max - x_min)*0.1
@@ -28,8 +27,8 @@ def draw_chart(data_series)
   y_max += (y_max - y_min)*0.1
   
 
-  svg_x = 500
-  svg_y = 500
+  svg_x = 1000
+  svg_y = 800
   svg = "<svg width='#{svg_x}' height='#{svg_y}' xmlns='http://www.w3.org/2000/svg'>"
     svg_x_range = x_max - x_min
     svg_x_scale = svg_x.to_f / svg_x_range
@@ -43,20 +42,29 @@ def draw_chart(data_series)
     #svg_y_0 = (1 - y_min) * svg_y_scale
     svg_y_0 = map_to_range(0, y_min, y_max, 0, svg_y)
     if y_min <= 0 and y_max >= 0
-      svg +=  "<line x1='0' y1='#{svg_y - svg_y_0}' x2='#{svg_y}' y2='#{svg_y - svg_y_0}' style='stroke:black;stroke-width:2' />"
+      svg +=  "<line x1='0' y1='#{svg_y - svg_y_0}' x2='#{svg_x}' y2='#{svg_y - svg_y_0}' style='stroke:black;stroke-width:2' />"
     end
 
-    data_series.each do |series|
-      series.each do |point|
-        # x = svg_x_0 + point[0] * svg_x_scale
-        # y = svg_y_0 + point[1] * svg_y_scale
-        x = map_to_range(point[0], x_min, x_max, 0, svg_x)
-        y = map_to_range(point[1], y_min, y_max, 0, svg_y)
-        svg += "<circle r='3' cx='#{x}' cy='#{svg_y - y}' fill='grey'>"
-        svg += "<title>"
-        svg += (point[2] + ": ") if point.size >=3
-        svg += "#{point[0]}, #{point[1]}</title>"
-        svg += "</circle>"
+    data_series.each do |point|
+      # x = svg_x_0 + point[0] * svg_x_scale
+      # y = svg_y_0 + point[1] * svg_y_scale
+      x = map_to_range(point[0], x_min, x_max, 0, svg_x)
+      y = map_to_range(point[1], y_min, y_max, 0, svg_y)
+      if point.size >=5
+        svg += "<a href='#{point[4]}'>"
+      end
+      svg += "<circle cx='#{x}' cy='#{svg_y - y}' " 
+      if point.size >=4
+        svg += "r='#{point[3][:rad]}' fill='#{point[3][:color]}' fill-opacity='#{point[3][:alpha]}'>"
+      else
+        svg += "r='3' fill='grey'>"
+      end
+      svg += "<title>"
+      svg += (point[2] + ": ") if point.size >=3
+      svg += "#{sprintf("%0.02f", point[0])}, #{sprintf("%0.02f", point[1])}</title>"
+      svg += "</circle>"
+      if point.size >=5
+        svg += "</a>"
       end
     end
   svg += "</svg>"
--- speed.rb
+++ speed.rb
@@ -2,42 +2,95 @@ require 'sinatra'
 require 'csv'
 require_relative 'scatter.rb'
 
-frames_file = "frames.csv"
-wheels_file = "wheels.csv"
 
-frames_raw = CSV.read(frames_file, headers: true)
-wheels_raw = CSV.read(wheels_file, headers: true)
+def get_data(selection={})
+  frames_file = "frames.csv"
+  wheels_file = "wheels.csv"
+  frames_raw = CSV.read(frames_file, headers: true)
+  wheels_raw = CSV.read(wheels_file, headers: true)
+  base_wheel = "Zwift 32mm Carbon"
+  base_bike = "Zwift Carbon"
 
-base_wheel = "Zwift 32mm Carbon"
-base_bike = "Zwift Carbon"
-
-bike_time = []
-wheel_time = []
-nowheel_bike_time = []
-
-frames_raw.each do |row|
-  bike = row["Bike"]
-  wheel = row["Wheels"]
-  next if ["Zwift Gravel", "Zwift Mountain", "Zwift Handcycle"].include? wheel
-  flat_gap = row["Flat Stage 0 - Hour Time Gap"].to_f
-  climb_gap = row["Climb Stage 0 - Hour Time Gap"].to_f
-  if wheel == base_wheel
-    bike_time << [flat_gap, climb_gap, bike]
-  elsif wheel == bike #specialty bikes
-    nowheel_bike_time << [flat_gap, climb_gap, bike]
+  bike_time = []
+  wheel_time = []
+  nowheel_bike_time = []
+  
+  frames_raw.each do |row|
+    bike = row["Bike"]
+    wheel = row["Wheels"]
+    next if ["Zwift Gravel", "Zwift Mountain"].include? wheel
+    next if ["Zwift BMX Bandit", "Zwift Safety", "Zwift Buffalo Fahrrad", "Zwift Handcycle", "Zwift Atomic Cruiser"].include? bike
+    flat_gap = row["Flat Stage 0 - Hour Time Gap"].to_f
+    flat5_gap = row["Flat Stage 5 - Hour Time Gap"].to_f
+    climb_gap = row["Climb Stage 0 - Hour Time Gap"].to_f
+    climb5_gap = row["Climb Stage 5 - Hour Time Gap"].to_f
+    if wheel == base_wheel
+      bike_time << [bike, flat_gap, climb_gap]
+      bike_time << ["#{bike} Stage 5", flat5_gap, climb5_gap]
+    elsif ["Cannondale R4000 Roller Blade", "Zwift Concept Z1", "Pinarello Espada", "Specialized PROJECT 74"].include? bike #specialty bikes
+      nowheel_bike_time << [bike, flat_gap, climb_gap]
+      nowheel_bike_time << ["#{bike} Stage 5", flat5_gap, climb5_gap]
+    end
   end
-end
-
-wheels_raw.each do |row|
-  bike = row["Bike"]
-  wheel = row["Wheels"]
-  flat_gap = row["Flat Hour Time Gap"].to_f
-  climb_gap = row["Climb Hour Time Gap"].to_f
-  if bike == base_bike
-    wheel_time << [wheel, flat_gap, climb_gap]
+  
+  wheels_raw.each do |row|
+    bike = row["Bike"]
+    wheel = row["Wheels"]
+    next if bike == "Zwift TT"
+    next if ["Zwift Atomic Cruiser 2024", "Zwift Buffalo Fahrrad", "Zwift Safety"].include? wheel
+    flat_gap = row["Flat Hour Time Gap"].to_f
+    climb_gap = row["Climb Hour Time Gap"].to_f
+    if bike == base_bike
+      wheel_time << [wheel, flat_gap, climb_gap]
+    end
   end
+  
+  combined_times = []
+  bike_time.each do |bike_row|
+    wheel_time.each do |wheel_row|
+      title = bike_row.first + "+" + wheel_row.first
+      link_url = "/bike/#{bike_row.first.split(" Stage 5").first}"
+      flat = bike_row[1] + wheel_row[1]
+      climb = bike_row[2] + wheel_row[2]
+      style={}
+      style[:rad] = 3
+      style[:color] = "grey"
+      style[:alpha] = "0.3"
+      if !!selection[:bike] and bike_row.first.start_with? selection[:bike]
+        style[:color] = "blue"
+        style[:rad] = 4
+        style[:alpha] = "1"
+        if bike_row.first.end_with? " Stage 5"
+          style[:color] = "purple"
+        end
+      elsif bike_row.first.end_with? " Stage 5"
+        style[:color] = "lightgreen"
+      end
+      combined_times << [flat, climb, title, style, link_url]
+    end
+  end
+  nowheel_bike_time.each do |time|
+    bike = time[0]
+    flat = time[1]
+    climb = time[2]
+    style={}
+    style[:rad] = 5
+    style[:color] = "red"
+    style[:alpha] = "1"
+    if bike.end_with? " Stage 5"
+      style[:color] = "pink"
+    end
+    combined_times << [flat, climb, bike, style]
+  end  
+  return combined_times
 end
 
 get '/' do
-  draw_chart([bike_time, nowheel_bike_time])
+  data = get_data()
+  draw_chart(data)
+end
+
+get '/bike/:bikename' do
+  data = get_data({bike: params[:bikename]})
+  draw_chart(data)
 end
